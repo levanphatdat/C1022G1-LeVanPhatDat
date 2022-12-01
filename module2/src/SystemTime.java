2ab65859a7c2f3fcd6b4a756fba5b8d17abbdd94
levanphatdat <levanphatdat9@gmail.com>
2022-12-01 17:03:10 +0700

    udate

--- module2/src/SystemTime.java
+++ module2/src/SystemTime.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+import java.util.Date;
 
 public class SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date(); // Lấy ra thời gian hiện tại
+
+        System.out.println("Now is: " + now);
+    }
 }
