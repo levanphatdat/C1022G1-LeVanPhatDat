517063e6846eae72040509ffeff7db4bf3a14294
levanphatdat <levanphatdat9@gmail.com>
2022-12-15 21:49:13 +0700

    baitap

--- module_2/src/ss10_DSA_stack_queue/bai_tap/Palindrome/Palindrome.java
+++ module_2/src/ss10_DSA_stack_queue/bai_tap/Palindrome/Palindrome.java
@@ -4,14 +4,14 @@ import java.util.*;
 
 public class Palindrome {
     public static void main(String[] args) {
-        Queue<Character> queue = new LinkedList<>();
+        Queue<String> queue = new LinkedList<>();
         Stack<String> stack = new Stack<>();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the String :");
-        String str = scanner.nextLine();
+        String str = scanner.nextLine().toLowerCase();
         for (int i = 0; i < str.length(); i++) {
-            stack.push(String.valueOf(str.charAt(i)));
-            queue.offer(str.charAt(i));
+            stack.push((String.valueOf(str.charAt(i))));
+            queue.offer(String.valueOf(str.charAt(i)));
         }
         boolean flag = true;
         for (int i = 0; i < str.length(); i++) {
@@ -24,5 +24,6 @@ public class Palindrome {
         if (flag) {
             System.out.println("String is a Palindrome.");
         }
+
     }
 }
