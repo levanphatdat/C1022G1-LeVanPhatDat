68e86d864e3034a9d55d0a1dc6198f604d3155ce
levanphatdat <levanphatdat9@gmail.com>
2022-12-31 19:16:32 +0700

    baitap

---
+++ module_2/src/bai_tap/case_study/Data/ReadWriteEmployee.java
@@ -0,0 +1,92 @@
+package bai_tap.case_study.Data;
+
+import bai_tap.case_study.Models.Person.Employee;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadWriteEmployee {
+
+    public static final String EMPLOYEE_CSV = "C:\\Users\\Admin\\Desktop\\CG\\module_2\\src\\bai_tap\\case_study\\Data\\employee.csv";
+
+    public List<Employee> readEmployee() {
+        List<Employee> employeeList = new ArrayList<>();
+        BufferedReader bufferedReader = null;
+        try {
+            bufferedReader = new BufferedReader(new FileReader(EMPLOYEE_CSV));
+            String line;
+            String[] arr;
+            Employee employee;
+            while ((line = bufferedReader.readLine()) != null) {
+                arr = line.split(",");
+                String id = arr[0];
+                String name = arr[1];
+                String dayOfBirth = arr[2];
+                String gender = arr[3];
+                String identityCard = arr[4];
+                String phoneNumber = arr[5];
+                String email = arr[6];
+                String qualifications = arr[7];
+                String position = arr[8];
+                String salary = arr[9];
+                employee = new Employee(id, name, dayOfBirth, gender, identityCard, phoneNumber, email, qualifications, position, salary);
+                employeeList.add(employee);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                bufferedReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return employeeList;
+    }
+
+    public void writeEmployee(List<Employee> employee) {
+        BufferedWriter bufferedWriter = null;
+        try {
+            bufferedWriter = new BufferedWriter(new FileWriter(EMPLOYEE_CSV));
+            for (Employee employee1 : employee) {
+                bufferedWriter.write(employee1.getId() + "," + employee1.getName() + "," + employee1.getDayOfBirth() + "," + employee1.getGender()
+                        + "," + employee1.getIdentityCard() + "," + employee1.getPhoneNumber() + "," + employee1.getEmail() + "," + employee1.getQualifications()
+                        + "," + employee1.getPosition() + "," + employee1.getSalary()+"\n");
+            }
+            bufferedWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                bufferedWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+    public void writeAppendEmployee(List<Employee> employee) {
+        BufferedWriter bufferedWriter = null;
+        try {
+            bufferedWriter = new BufferedWriter(new FileWriter(EMPLOYEE_CSV,true));
+            for (Employee employee1 : employee) {
+                bufferedWriter.write(employee1.getId() + "," + employee1.getName() + "," + employee1.getDayOfBirth() + "," + employee1.getGender()
+                        + "," + employee1.getIdentityCard() + "," + employee1.getPhoneNumber() + "," + employee1.getEmail() + "," + employee1.getQualifications()
+                        + "," + employee1.getPosition() + "," + employee1.getSalary());
+            }
+            bufferedWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                bufferedWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
--- module_2/src/bai_tap/case_study/Repository/EmployeeRepository/EmployeeRepository.java
+++ module_2/src/bai_tap/case_study/Repository/EmployeeRepository/EmployeeRepository.java
@@ -1,6 +1,6 @@
 package bai_tap.case_study.Repository.EmployeeRepository;
 
-import bai_tap.case_study.Data.RAWEmployee;
+import bai_tap.case_study.Data.ReadWriteEmployee;
 import bai_tap.case_study.Models.Person.Employee;
 
 import java.util.ArrayList;
@@ -8,41 +8,36 @@ import java.util.List;
 
 public class EmployeeRepository implements IEmployeeRepository {
     public static List<Employee> employeeList = new ArrayList<>();
-    static RAWEmployee readWriteEmployee=new RAWEmployee();
-
-//    static {
-//        employeeList.add(new Employee("NV-1", "Truong", "25/12/2022", "male", "191925684", "0923654123", "truong123@gmail.com", "After university", "Director", "50000000"));
-//        employeeList.add(new Employee("NV-2", "Dat", "12/02/1999", "male", "191925341", "0936452145", "datcute@gmail.com", "university", "manager", "30000000"));
-//        employeeList.add(new Employee("NV-3", "Khai", "05/07/2000", "male", "191936202", "0983242364", "khailor@gmail.com", "intermediate", "receptionist", "5000000"));
-//    }
+    public static ReadWriteEmployee readWriteEmployee = new ReadWriteEmployee();
 
     @Override
     public void add(Employee employee) {
         employeeList.add(employee);
-        readWriteEmployee.writeEmployee(employeeList);
+        readWriteEmployee.writeAppendEmployee(employeeList);
     }
 
     @Override
     public void display() {
-        readWriteEmployee.readEmployee();
-        for (Employee employee : employeeList) {
+        List<Employee> employees = readWriteEmployee.readEmployee();
+        for (Employee employee : employees) {
             System.out.println(employee);
         }
     }
 
     @Override
     public void delete(String id) {
-        List<Employee> employees=readWriteEmployee.readEmployee();
+        List<Employee> employees = readWriteEmployee.readEmployee();
         for (int i = 0; i < employees.size(); i++) {
-            if (employees.get(i).getId().equals(id)){
+            if (employees.get(i).getId().equals(id)) {
                 employees.remove(employees.get(i));
             }
         }
+        readWriteEmployee.writeEmployee(employees);
     }
 
     @Override
     public Employee checkID(String id) {
-       List<Employee> employees= readWriteEmployee.readEmployee();
+        List<Employee> employees = readWriteEmployee.readEmployee();
         for (Employee employee : employees) {
             if (employee.getId().equals(id)) {
                 return employee;
@@ -53,11 +48,12 @@ public class EmployeeRepository implements IEmployeeRepository {
 
     @Override
     public void edit(Employee employee) {
-        List<Employee> employees=readWriteEmployee.readEmployee();
+        List<Employee> employees = readWriteEmployee.readEmployee();
         for (int i = 0; i < employees.size(); i++) {
             if (employees.get(i).getId().equals(employee.getId())) {
                 employees.set(i, employee);
             }
         }
+        readWriteEmployee.writeEmployee(employees);
     }
 }
